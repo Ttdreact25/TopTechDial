5323092b71b6ddc2b7723f0cee5c1342b7590e66
TopTechDial Administrator <ttdreact@gmail.com>
2026-03-30 18:27:09 +0530

    colour update needed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -213,7 +213,7 @@ const styles = {
     textAlign: 'center',
     padding: '32px 20px',
     borderRadius: '20px',
-    background: 'rgba(255,255,255,0.03)',
+    background: 'rgba(18, 16, 16, 0.03)',
     border: '1px solid rgba(255,255,255,0.05)',
   },
   iconWrapper: {
