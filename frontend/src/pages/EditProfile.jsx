427fbda3d6bd5201ccfdd8b5fa85133ed4169ac7
TopTechDial Administrator <ttdreact@gmail.com>
2026-03-30 19:07:50 +0530

    colour update needed

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -330,7 +330,7 @@ const styles = {
     justifyContent: 'center',
     gap: '10px',
     cursor: 'pointer',
-    boxShadow: '0 8px 16px rgba(254, 90, 49, 0.3)',
+    boxShadow: '0 8px 16px rgba(251, 86, 45, 0.3)',
     transition: 'all 0.3s',
   }
 };
